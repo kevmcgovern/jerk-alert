4a84d70b822bf4864c52a9527cef82f004856f07
Kevin M <kevin.mcgovern92@gmail.com>
2016-08-13 13:52:26 -0700

    Add associations between post and user models; add user auth methods from bcrypt

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+require 'bcrypt'
+
+class User < ActiveRecord::Base
+  include BCrypt
+
+  has_many :posts
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
+
+end
