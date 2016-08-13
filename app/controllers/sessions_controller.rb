26149f35fd2bf53ba030114add36aa30a9fecd0f
Kevin M <kevin.mcgovern92@gmail.com>
2016-08-13 14:20:20 -0700

    Add RESTful routes for sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+# Sessions NEW
+get '/sessions/new' do
+	erb :'sessions/new'
+end
+
+
+# Sessions CREATE
+post '/sessions' do
+	@user = User.find_by(username: params[:user][:username])
+	if @user
+		if @user.authenticate(params[:user][:password])
+			session[:id] = @user.id
+			redirect '/'
+		else
+			@error = "invalid login information. Please try again."
+			redirect '/sessions/new'
+	end
+else
+	@error = "You are not registered..... yet."
+	redirect '/users/new'
+end
+
+
+# Sessions DESTROY
+delete '/sessions/:id' do
+	session[:id] = nil
+	redirect '/'
+end
