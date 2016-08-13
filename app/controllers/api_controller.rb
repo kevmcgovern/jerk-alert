bc4e304796505c39a78adb9a2d35241fbea216cb
Kevin M <kevin.mcgovern92@gmail.com>
2016-08-13 13:29:23 -0700

    Add API query to helper method; comment out duplicate code in api_controller

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,25 +1,29 @@
-require_relative '../models/api_model.rb'
+# require_relative '../models/api_model.rb'
 
-demo_text = "The Alabama Shakes show last night was absolutely awesome! Such an incredible show!"
+# demo_text = "The Alabama Shakes show last night was absolutely awesome! Such an incredible show!"
 
-alchemyapi = AlchemyAPI.new()
+# alchemyapi = AlchemyAPI.new()
 
-puts "Making calls on: " + demo_text
+# puts "Making calls on: " + demo_text
 
-response = alchemyapi.sentiment_targeted('text', demo_text, 'Alabama Shakes')
+# response = alchemyapi.sentiment_targeted('text', demo_text, 'Alabama Shakes')
 
-if response['status'] == 'OK'
-	puts 'We gucci. Response object inbound'
-	puts JSON.pretty_generate(response)
+# if response['status'] == 'OK'
+# 	puts 'We gucci. Response object inbound'
+# 	puts JSON.pretty_generate(response)
 
-	puts "Targeted sentiment analysis forthcoming"
-	puts "type: " + response['docSentiment']['type']
+# 	puts "Targeted sentiment analysis forthcoming"
+# 	puts "type: " + response['docSentiment']['type']
 
-	if response['docSentiment'].key?('score')
-		puts 'score: ' + response['docSentiment']['score']
-	end
+# 	if response['docSentiment'].key?('score')
+# 		puts 'score: ' + response['docSentiment']['score']
+# 	end
 
-else
-	puts "You done fucked up! " #+ response['statusInfo']
-	puts response
+# else
+# 	puts "You done fucked up! " + response['statusInfo']
+# 	puts response
+# end
+
+post '/posts' do
+	redirect '/'
 end
