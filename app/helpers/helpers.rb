7ac0f2ed6ca3e5159fc8c965a521d3e0b40c379f
Kevin M <kevin.mcgovern92@gmail.com>
2016-08-14 11:24:44 -0700

    Add new query method to helpers, comment out V1.5 query

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -2,27 +2,42 @@ require_relative '../models/api_model.rb'
 
 helpers do
 
-  def alchemy_query(query_text, query_target)
+  # This query type will be a stretch feature; not MVP
+  # def alchemy_query(query_text, query_target)
+  #   alchemyapi = AlchemyAPI.new()
+
+  #   puts "Making calls "
+
+  #   response = alchemyapi.sentiment_targeted('text', query_text, query_target)
+
+  #   if response['status'] == 'OK'
+  #     puts 'All clear. Response object inbound'
+  #     puts JSON.pretty_generate(response)
+
+  #     puts "Targeted sentiment analysis forthcoming"
+  #     puts "type: " + response['docSentiment']['type']
+
+  #     if response['docSentiment'].key?('score')
+  #       puts 'score: ' + response['docSentiment']['score']
+  #     end
+
+  #   else
+  #     puts "That was an invalid request: " + response['statusInfo']
+  #     puts response
+  #   end
+  # end
+
+  def sentiment_query(query_text)
     alchemyapi = AlchemyAPI.new()
-
-    puts "Making calls "
-
-    response = alchemyapi.sentiment_targeted('text', query_text, query_target)
-
+    response = alchemyapi.sentiment('text', query_text)
     if response['status'] == 'OK'
-      puts 'All clear. Response object inbound'
+      puts "Generating response"
       puts JSON.pretty_generate(response)
-
-      puts "Targeted sentiment analysis forthcoming"
-      puts "type: " + response['docSentiment']['type']
-
       if response['docSentiment'].key?('score')
         puts 'score: ' + response['docSentiment']['score']
       end
-
     else
-      puts "That was an invalid request: " + response['statusInfo']
-      puts response
+      puts 'Error in sentiment call: ' + response['statusInfo']
     end
   end
 
