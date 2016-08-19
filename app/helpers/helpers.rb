1081f851543d0c4dc9c1a0d146b1a2a44eddd43b
Kevin M <kevin.mcgovern92@gmail.com>
2016-08-18 17:45:29 -0700

    Add targeted sentiment method

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -27,6 +27,31 @@ helpers do
   #   end
   # end
 
+
+
+  def targeted_sentiment_query(query_text, query_target)
+    alchemyapi = AlchemyAPI.new()
+    response = alchemyapi.sentiment_targeted('text', query_text, query_target)
+    if response['status'] == 'OK'
+      puts "Generating response"
+      puts JSON.pretty_generate(response)
+      if response['docSentiment'].key?('score')
+        puts 'score: ' + response['docSentiment']['score']
+      end
+    else
+      puts 'Error in sentiment call: ' + response['statusInfo']
+    end
+    return response
+  end
+
+  def targeted_sentiment_parse(query_response)
+    sentiment = query_response['docSentiment']['type']
+    score = (query_response['docSentiment']['score'].to_f) * 100
+    return { sentiment: sentiment, score: score }
+    # return [sentiment, score] 
+  end
+
+
   def sentiment_query(query_text)
     alchemyapi = AlchemyAPI.new()
     response = alchemyapi.sentiment('text', query_text)
@@ -45,7 +70,8 @@ helpers do
   def sentiment_parse(query_response)
     sentiment = query_response['docSentiment']['type']
     score = (query_response['docSentiment']['score'].to_f) * 100
-    "The sentiment of your query is #{sentiment}, and the score is #{score}."
+    return { sentiment: sentiment, score: score }
+    # return [sentiment, score]
   end
 
   def emotion_query(query_text)
@@ -70,16 +96,15 @@ helpers do
     fear = (query_response['docEmotions']['fear'].to_f) * 100
     joy = (query_response['docEmotions']['joy'].to_f) * 100
     sadness = (query_response['docEmotions']['sadness'].to_f) * 100
-    "The emotional scores of your query are as follows: 
-      anger: #{anger}
-      disgust: #{disgust} 
-      fear: #{fear} 
-      joy: #{joy}
-      sadness: #{sadness}"
+    return { anger: anger, disgust: disgust, fear: fear, joy: joy, sadness: sadness }
   end
 
   def current_user
     @current_user ||= User.find(session[:id]) if session[:id]
+  end
+
+  def string_to_hash(post_analysis)
+    return eval(post_analysis)
   end
 
 end
